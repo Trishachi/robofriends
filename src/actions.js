9f6a973c8edc39d61e09f61ad19e7e858d8a90ea
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-08-19 09:40:27 -0600

    Redux Async Actions

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,19 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import { CHANGE_SEARCH_FIELD,
+         RESQEST_ROBOTS_PENDING,
+         RESQEST_ROBOTS_SUCCESS,
+         RESQEST_ROBOTS_FAILED
+} from './constants.js';
 
 export const setSearchField = (text) => ({
         type: "CHANGE_SEARCH_FIELD",
         payload: text
 })
+
+export const requestRobots = () => (dispatch) => {
+        dispatch({ type: RESQEST_ROBOTS_PENDING });
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(data => dispatch({ type: RESQEST_ROBOTS_SUCCESS, payload: data }))
+            .catch(error => dispatch({type: RESQEST_ROBOTS_FAILED, payload: error }))
+}
+
--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,38 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import { CHANGE_SEARCH_FIELD,
+    RESQEST_ROBOTS_PENDING,
+    RESQEST_ROBOTS_SUCCESS,
+    RESQEST_ROBOTS_FAILED
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
     searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
       switch(action.type){
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, { searchField:action.payload });
         default:
             return state;
     }
+}
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [], 
+    error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+    switch(action.type) {
+        case RESQEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true })
+        case RESQEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+        case RESQEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false })
+        default:
+            return state;
+    }
+
 }
