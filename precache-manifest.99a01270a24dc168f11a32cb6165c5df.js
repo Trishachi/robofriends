e827fd2f49b9252878f1b4e7da8a4ea9e54490dd
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-12 12:05:11 -0700

    Updates

---
+++ precache-manifest.99a01270a24dc168f11a32cb6165c5df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528c4ed44c47a406b8c4eb9f6e5f9222",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "731687b90d746c007f48",
+    "url": "/robofriends/static/css/main.6ebcecee.chunk.css"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends/static/js/2.e93fd954.chunk.js"
+  },
+  {
+    "revision": "731687b90d746c007f48",
+    "url": "/robofriends/static/js/main.2dc16324.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
